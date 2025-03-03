53bee08e9cc56b80b43eecc27800a8a5320c4150
Mostafa Saad <154759328+mostafa-s-mostafa@users.noreply.github.com>
2025-03-03 15:12:24 +0200

    upadates the model

--- pages/3_user_input.py
+++ pages/3_user_input.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from imblearn.pipeline import Pipeline as ImbPipeline
 import joblib
 
 
--- pages/4_chatbot.py
+++ pages/4_chatbot.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import imblearn
 import joblib  
 
 st.title("🤖 Churn Chatbot")
